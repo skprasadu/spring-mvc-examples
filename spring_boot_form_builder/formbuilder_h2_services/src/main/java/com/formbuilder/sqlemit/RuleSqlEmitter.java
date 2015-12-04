8f3702b759ad2d094090c9f7e1d66a9444eb436f
unknown <vu149d@A5695194.nw.nos.boeing.com>
2015-12-04 10:24:12 -0800

    modified database emit part

--- spring_boot_form_builder/formbuilder_h2_services/src/main/java/com/formbuilder/sqlemit/RuleSqlEmitter.java
+++ spring_boot_form_builder/formbuilder_h2_services/src/main/java/com/formbuilder/sqlemit/RuleSqlEmitter.java
@@ -1,25 +1,28 @@
 package com.formbuilder.sqlemit;
 
 public class RuleSqlEmitter extends SqlEmitter {
-	
+
 	@Override
-	public void emit(String appName, String tableName, String[] column, String[] relationship, int orderBy, StringBuffer ddlScripts, StringBuffer dmlScripts) {
-		ddlScripts.append(String.format(openCreate, tableName) + System.lineSeparator());
+	public void emit(String appName, String tableName, String[] column, String[] relationship, int orderBy, StringBuffer ddlScripts,
+			StringBuffer dmlScripts) {
+		ddlScripts.append(String.format("CREATE Table %s (", tableName) + System.lineSeparator());
 		String nameColumn = "name";
 		for (String col : column) {
 			String[] st = col.split(":");
-			if(st.length == 2){
+			if (st.length == 2) {
 				nameColumn = st[1];
 			}
-			setVal(ddlScripts, st[0]);
-			ddlScripts.append("," + System.lineSeparator());
 		}
-		
-		int i =0;
+
+		int i = 0;
 		for (String rel : relationship) {
 			String[] st = rel.split(":");
 			String relName = st.length == 1 ? st[0] : st[1] + "__" + st[0];
-			ddlScripts.append(String.format(relIdColumn, relName) );
+			if (i > 0) {
+				ddlScripts.append(String.format(relIdColumn, relName));
+			} else {
+				ddlScripts.append(String.format(relIdColumn, relName) + " Not Null");
+			}
 			if (i != relationship.length - 1) {
 				ddlScripts.append(",");
 			}
@@ -28,8 +31,8 @@ public class RuleSqlEmitter extends SqlEmitter {
 		}
 
 		ddlScripts.append(closeCreate + System.lineSeparator());
-		
-		//Insert Script
+
+		// Insert Script
 		insertUiForm(dmlScripts, tableName, nameColumn, orderBy, "Rule");
 	}
 }
